003ded021a9ea71139f2838d183cc26e7012be62
sjimen13 <sjimen13@gmail.com>
2021-09-11 17:44:34 -0500

    init-project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
   { path: '', redirectTo: '/admin', pathMatch: 'full' },
@@ -9,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
